4558386f431ee8100117dec0861455b6a16aed4d
Nhu2825 <nhunguyen.31231023430@st.ueh.edu.vn>
2024-11-26 19:44:30 +0700

    update

--- CSLT/Bonus/MultidimensionalArraysEx.cs
+++ CSLT/Bonus/MultidimensionalArraysEx.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace CSLT.Bonus
 {
@@ -12,10 +13,19 @@ namespace CSLT.Bonus
         {
             Console.ReadKey();
         }
+        /// <summary>
+        /// Write a program in C# Sharp for a 2D array of size 3x3 and print the matrix.
+        /// </summary>
         static void ex18()
         {
-
+            int[,] matrix = new int[3, 3];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.WriteLine($"element - [{i},{j}]: "); Console.ReadLine();
+                }
+            }
         }
-
     }
 }
